230ddc4f3801ee266f107158dd0716fd6ff6c20b
ChangYeon Hong <hongchangyeon@gmail.com>
2020-06-06 12:00:37 -0500

    initial commit with Yelp API

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,36 @@ var map, infoWindow;
 var originLat, originLong;
 
 var geo = confirm("Do you want to use geolocation?");
+var askOrigin = "10098+Red+Sage+Dr,+Colorado+Springs,+CO+80920";
+
+var askDestiation = "Garden+of+the+gods"
+var askMode = "";
+
+
+
+  $.ajax({
+    url:
+      "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=foodtrucks&location=austin&limit=50",
+    method: "GET",
+    timeout: 0,
+    headers: {
+      Authorization:
+        "Bearer LWQKKIC6WZLlwyI7-Pg9pE02R0wCWEQtyFS7Y3hPAu-PEbUHgvd9P9munA-Ozw5qz4XsU-RlmxcW9o8bzippVqsR-MUSpI5ZOeEs4J25asF8SJYQZZNbZFefcZPSXnYx",
+      // Cookie:
+      //   "hl=en_US; wdi=1|53E4B0D737ADFACD|0x1.7b4a3c92c2739p+30|e19930740c790522"
+    },
+  }).then(function (getYelpApi) {
+    console.log(getYelpApi);
+  });
+
+
+
+
+
+
 
 
 function initMap() {
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30.270575, lng: -97.744214},
     zoom: 12
@@ -30,60 +56,49 @@ function initMap() {
             infoWindow.setContent('Current Location.');
             infoWindow.open(map);
             map.setCenter(pos);
+
+            var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?latlng="+ originLat +","+originLong+"&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk"
+            $.ajax({
+                url: queryURL,
+                method: "POST",
+                dataType: "json",
+                header: {
+                'Access-Control-Allow-Origin':'*'
+                    }
+    
+              }).then(function(response){
+                   console.log(response.results[0].formatted_address);
+                   origin = response.results[0].formatted_address;
+                   var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin="+ origin +"&destination=Garden+of+the+gods&mode=driving&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
+                   $.ajax({
+                     
+                 url: queryURL,
+                 method: "GET",
+                 dataType: "json",
+                 header: {
+                     'Access-Control-Allow-Origin':'*'
+                 }
+               }).then(function(response2) {
+                 console.log(response2);
+               });  
+
+          
+    
+        });
+
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
+          
+
         } else {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
       } else {
-        origin = "10098+Red+Sage+Dr,+Colorado+Springs,+CO+80920"
-      }
+        origin = askOrigin;
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
-
-
-
-  if (geo == true) {
-  infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            originLat = pos.lat;
-            originLong = pos.lng;
-            console.log(originLat);
-            console.log(originLong);
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Current Location.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-
-
-            var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ originLat +","+originLong+"&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
-
-    $.ajax({
-      url: queryURL,
-        method: "GET",
-        dataType: "json",
-        header: {
-            'Access-Control-Allow-Origin':'*'
-        }
-
-    }).then(function(response){
-          console.log(response.results[0].formatted_address);
-          var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin="+ response.results[0].formatted_address+"&destination=Garden+of+the+gods&mode=driving&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
+        var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?location="+ origin +"&destination=Garden+of+the+gods&mode=driving&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
           $.ajax({
         url: queryURL,
         method: "GET",
@@ -91,29 +106,9 @@ function initMap() {
         header: {
             'Access-Control-Allow-Origin':'*'
         }
-      }).then(function(response2) {
-        console.log(response2);
+      }).then(function(response3) {
+        console.log(response3);
       });
-      
-
-    });
-
-
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      } else {
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 30.270575, lng: -97.744214},
-          zoom: 12
-      
-      
-        });
         
       }
 
@@ -126,121 +121,7 @@ function initMap() {
       }
 
 
-
-    
 }
 
-// by using cors-anywhere, CO
-
-  // function getDirections() {
-
-  //   // if (geo === true) {
-  //   //   console.log(originLat)
-
-  //   var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ originLat +","+originLong+"&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
-
-  //   $.ajax({
-  //     url: queryURL,
-  //       method: "GET",
-  //       dataType: "json",
-  //       header: {
-  //           'Access-Control-Allow-Origin':'*'
-  //       }
-
-  //   }).then(function(response){
-  //         console.log(response);
-  //         var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin="+ response+"&destination=Garden+of+the+gods&mode=driving&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
-  //         $.ajax({
-  //       url: queryURL,
-  //       method: "GET",
-  //       dataType: "json",
-  //       header: {
-  //           'Access-Control-Allow-Origin':'*'
-  //       }
-  //     }).then(function(response2) {
-  //       console.log(response2);
-  //     });
-      
-// 
-    // });
-  // } else {
-
-  //   var queryURL ="https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?location="+ origin +"&destination=Garden+of+the+gods&mode=driving&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk";
-  //         $.ajax({
-  //       url: queryURL,
-  //       method: "GET",
-  //       dataType: "json",
-  //       header: {
-  //           'Access-Control-Allow-Origin':'*'
-  //       }
-  //     }).then(function(response3) {
-  //       console.log(response3);
-  //     });
 
 
-  // }
-
-      
-
-    // $.get("https://maps.googleapis.com/maps/api/directions/json?origin=10098+Red+Sage+Dr&destination=Garden+of+the+gods&mode=transit&key=AIzaSyAHeXe0OoBIReOvCuEJq5cnU3LhVahYTAk").done(function (data) {
-    // console.log(data);
-
-
-    // };
-
-
-
-
-
-// function initMap() {
-  
-    
-//    var pointA = "4313+Captain+Jack+Ln"
-//    var pointB = "10098+Red+Sage+Dr"
-
-//     // initial= new google.maps.LatLng(30.2672, 97.7431);
-//     myOptions = {
-//       zoom: 7,
-//       center: initial
-//     },
-//     map = new google.maps.Map(document.getElementById('map'), myOptions),
-//     // Instantiate a directions service.
-//     directionsService = new google.maps.DirectionsService,
-//     directionsDisplay = new google.maps.DirectionsRenderer({
-//       map: map
-//     }),
-//     markerA = new google.maps.Marker({
-//       position: pointA,
-//       title: "point A",
-//       label: "A",
-//       map: map
-//     }),
-//     markerB = new google.maps.Marker({
-//       position: pointB,
-//       title: "point B",
-//       label: "B",
-//       map: map
-//     });
-
-//   // get route from A to B
-//   calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
-
-// }
-
-
-
-// function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
-//   directionsService.route({
-//     origin: pointA,
-//     destination: pointB,
-//     travelMode: google.maps.TravelMode.TRANSIT
-//   }, function(response, status) {
-//     if (status == google.maps.DirectionsStatus.OK) {
-//       directionsDisplay.setDirections(response);
-//     } else {
-//       window.alert('Directions request failed due to ' + status);
-//     }
-//   });
-// }
-
-// initMap();
